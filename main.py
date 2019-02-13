1f838c76453dd07f5d30141a69da3cd24f5257ef
wangyonghui <2930807240@qq.com>
2019-02-13 22:11:28 +0800

    init

--- main.py
+++ main.py
@@ -2,5 +2,11 @@
 __author__ = 'alex'
 
 import pandas as pd
-df=pd.read_csv("./data/HR.csv")
+
+df = pd.read_csv("./data/HR.csv")
+type(df['satisfaction_level'])
+type(df.mean())
+df.mean()
+df.median()
+df.quantile(0.25)
 print(df.head(10))
